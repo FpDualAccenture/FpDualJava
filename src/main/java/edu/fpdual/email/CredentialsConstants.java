e69f14402d7f5c5fe6615b0cee7e0aced1af2723
FpDualAccenture <127385697+FpDualAccenture@users.noreply.github.com>
2023-03-09 09:39:08 +0100

    Creating Repository

---
+++ src/main/java/edu/fpdual/email/CredentialsConstants.java
@@ -0,0 +1,14 @@
+package edu.fpdual.email;
+
+/**
+ * credentials Constants class.
+ * @author jose.m.prieto.villar
+ *
+ */
+public class CredentialsConstants {
+
+    public static final String PASSWD = "mail.smtp.password";
+    public static final String USER = "mail.smtp.user";
+
+}
+
